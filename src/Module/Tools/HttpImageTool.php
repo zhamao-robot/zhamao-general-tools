d284c6eeb82f251f33472ca842737324f874d9e0
Whale <jesse2061@outlook.com>
2020-04-18 18:57:25 +0800

    Update HttpImageTool.php

--- src/Module/Tools/HttpImageTool.php
+++ src/Module/Tools/HttpImageTool.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace Module\Tools;
+namespace Module\Manager;
 
 
 use Co;
@@ -9,21 +9,24 @@ use Framework\ZMBuf;
 use ZM\Annotation\Http\RequestMapping;
 use ZM\Annotation\Swoole\SwooleEventAfter;
 use ZM\ModBase;
+use ZM\Utils\ZMRequest;
+use ZM\Utils\ZMUtil;
 
 class HttpImageTool extends ModBase
 {
+    const FILE_PATH = ZM_DATA . "images/";
     /**
      * @SwooleEventAfter("workerStart")
      */
     public function onWorkerStart(){
-        @mkdir(ZM_DATA."images/");
+        @mkdir(self::FILE_PATH);
     }
     /**
      * @param $param
      * @RequestMapping("/images/{file_name}")
      */
     public function innerImage($param) {
-        $filename = ZM_DATA . "images/";
+        $filename = self::FILE_PATH;
         $param_name = $param["file_name"];
         $param_name = strtolower($param_name);
         if (mb_strpos($param_name, "..") !== false) {
@@ -35,5 +38,28 @@ class HttpImageTool extends ModBase
         if (($type = array_pop($exp)))
             $this->response->header("Content-Type", ZMBuf::config("file_header")[$type] ?? "text/html");
         $this->response->end(Co::readFile($filename.$param_name));
+    }
+
+    public static function downloadImageFromCQ(&$msg) {
+        $files = [];
+        $rec = 0;
+        while(($cq = ZMUtil::getCQ($msg)) !== null) {
+            ++$rec;
+            if($rec > 5) break;
+            if ($cq === null) return [];
+            if ($cq["type"] == "image") {
+                $url = $cq["params"]["url"] ?? null;
+                if ($url === null) return [];
+                $file = ZMRequest::get($url);
+                if($file === false) continue;
+                file_put_contents(self::FILE_PATH . $cq["params"]["file"], $file);
+                $cqs = mb_strpos($msg, "[");
+                $cqe = mb_strpos($msg, "]");
+                $sub = mb_substr($msg, $cqs, $cqe + 1);
+                $msg = str_replace($sub, "{{img:" . $cq["params"]["file"] . "}}", $msg);
+                $files[]=$cq["params"]["file"];
+            }
+        }
+        return $files;
     }
 }
